f3790e62ccf377ce071562e64a69f4384e692c5e
Stigiboy <stg@hotmail.no>
2017-02-10 17:48:55 +0100

    Fixed so that it doesnt create a new socket connection everytime it sends data

--- app/src/main/java/com/example/nichlasanynes/mindstormapp/MainActivity.java
+++ app/src/main/java/com/example/nichlasanynes/mindstormapp/MainActivity.java
@@ -4,12 +4,18 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -21,67 +27,49 @@ public class MainActivity extends AppCompatActivity {
 
     Button tryBluetoothButton;
     BluetoothSocket mmSocket = null;
+    BluetoothSocket fallbackSocket;
     BluetoothDevice mmDevice = null;
     BluetoothAdapter mBluetoothAdapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     public void controlRobot(View view) {
 
-        Button btn = (Button) view; 
-        String command = btn.getTag().toString(); 
-        
+        Button btn = (Button) view;
+        String command = btn.getTag().toString();
+        Toast.makeText(getApplicationContext(), command, Toast.LENGTH_SHORT).show();
+
         sendCommand(command);
     }
 
     public void sendCommand(String command) {
         UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
 
-        try {
-           // mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        // mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
 //            mmSocket = mmDevice.createInsecureRfcommSocketToServiceRecord(uuid);
 //            Toast.makeText(this, "createRfcommSocketToServiceRecord " + uuid, Toast.LENGTH_SHORT).show();
 //            mmSocket.connect();
 //            Log.i("myStuff", "Conected OK!");
 //            Toast.makeText(this, "connected", Toast.LENGTH_SHORT).show();
 
-            mmSocket =(BluetoothSocket) mmDevice.getClass().getMethod("createRfcommSocket", new Class[] {int.class}).invoke(mmDevice,1);
-            Log.d("DEBUG CRASH", "mmSocket");
-            Class<?> clazz = mmSocket.getRemoteDevice().getClass();
-            Log.d("DEBUG CRASH", "clazz");
-            Class<?>[] paramTypes = new Class<?>[] {Integer.TYPE};
-            Log.d("DEBUG CRASH", "paramTypes");
-            Method m = clazz.getMethod("createRfcommSocket", paramTypes);
-            Log.d("DEBUG CRASH", "getMethod");
-            Object[] params = new Object[] {Integer.valueOf(1)};
-            Log.d("DEBUG CRASH", "newobject[]");
+        if (fallbackSocket != null) {
 
-            BluetoothSocket fallbackSocket = (BluetoothSocket) m.invoke(mmSocket.getRemoteDevice(), params);
-            Log.d("DEBUG CRASH", "fallbackSocket");
-            fallbackSocket.connect();
-
-            if(fallbackSocket != null) {
-
-                try {
-                    fallbackSocket.getOutputStream().write(command.getBytes());
-                    fallbackSocket.getOutputStream().flush();
-                    fallbackSocket.close();
-                } catch(IOException e) {
-                    Log.e("IOExceptoin", e.getMessage().toString());
-                }
-
-            } else {
-                Log.e("ERROR", "fallbackSocket is null");
+            try {
+                fallbackSocket.getOutputStream().write(command.getBytes());
+                fallbackSocket.getOutputStream().flush();
+               // fallbackSocket.close();
+            } catch (IOException e) {
+                Log.e("IOExceptoin", "Error message: " + e.getMessage().toString());
             }
 
-        } catch (IOException e) {
-
-            Log.i("myStuff", "EXCEPTION THROWN: " + e.getMessage().toString());
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        } else {
+            Log.e("ERROR", "fallbackSocket is null");
+            Toast.makeText(this, "FallbackSocket is null", Toast.LENGTH_SHORT).show();
         }
+
     }
 
     @Override
@@ -108,8 +96,7 @@ public class MainActivity extends AppCompatActivity {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         if (pairedDevices.size() > 0) {
             for (BluetoothDevice device : pairedDevices) {
-                if (device.getName().equals("13516c2499dc"))
-                {
+                if (device.getName().equals("13516c2499dc")) {
                     mmDevice = device;
                     Log.i("myStuff", "Device equals " + device.getName());
                     Toast toast = Toast.makeText(this, "Device equals " + device.getName(), Toast.LENGTH_LONG);
@@ -121,5 +108,84 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
+
+        try {
+            mmSocket = (BluetoothSocket) mmDevice.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(mmDevice, 1);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG CRASH", "mmSocket");
+        Class<?> clazz = mmSocket.getRemoteDevice().getClass();
+        Log.d("DEBUG CRASH", "clazz");
+        Class<?>[] paramTypes = new Class<?>[]{Integer.TYPE};
+        Log.d("DEBUG CRASH", "paramTypes");
+        Method m = null;
+        try {
+            m = clazz.getMethod("createRfcommSocket", paramTypes);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG CRASH", "getMethod");
+        Object[] params = new Object[]{Integer.valueOf(1)};
+        Log.d("DEBUG CRASH", "newobject[]");
+
+        fallbackSocket = null;
+        try {
+            fallbackSocket = (BluetoothSocket) m.invoke(mmSocket.getRemoteDevice(), params);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG CRASH", "fallbackSocket");
+        try {
+            fallbackSocket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
