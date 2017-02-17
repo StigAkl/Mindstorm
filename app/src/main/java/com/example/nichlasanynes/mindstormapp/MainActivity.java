148864c5205e679a4db467d19de04cfe856a2cb1
Stigiboy <stg@hotmail.no>
2017-02-17 15:05:33 +0100

    Implemented simple user interface to control the robot, text to speech and onTouchListeners

--- app/src/main/java/com/example/nichlasanynes/mindstormapp/MainActivity.java
+++ app/src/main/java/com/example/nichlasanynes/mindstormapp/MainActivity.java
@@ -8,8 +8,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -21,7 +26,6 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Set;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
     BluetoothSocket fallbackSocket;
     BluetoothDevice mmDevice = null;
     BluetoothAdapter mBluetoothAdapter;
+
+    ImageView forward;
+    ImageView backward;
+    ImageView turnRight;
+    ImageView turnLeft;
+
+    //Buttons
+    //ImageView forward = (ImageView) findViewById(R.id.forward);
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -38,22 +50,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void controlRobot(View view) {
 
-        Button btn = (Button) view;
-        String command = btn.getTag().toString();
+        ImageView controlButton = (ImageView) view;
+        String command = controlButton.getTag().toString();
         Toast.makeText(getApplicationContext(), command, Toast.LENGTH_SHORT).show();
-
         sendCommand(command);
     }
 
     public void sendCommand(String command) {
-        UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
-
         // mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
 //            mmSocket = mmDevice.createInsecureRfcommSocketToServiceRecord(uuid);
 //            Toast.makeText(this, "createRfcommSocketToServiceRecord " + uuid, Toast.LENGTH_SHORT).show();
 //            mmSocket.connect();
 //            Log.i("myStuff", "Conected OK!");
 //            Toast.makeText(this, "connected", Toast.LENGTH_SHORT).show();
+
+        //Protocol: If the robot receives a message starting with "#", the message is sent to
+        //the robots Text to Speech module
+        if(command.equals("textToSpeech")) {
+            EditText textToSpeech = (EditText) findViewById(R.id.textToSpeechText);
+            command = "#"+textToSpeech.getText().toString();
+            Toast.makeText(getApplicationContext(), command, Toast.LENGTH_SHORT).show();
+        }
 
         if (fallbackSocket != null) {
 
@@ -76,8 +93,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showLoading();
+        hideControls();
 
+        try {
+            Thread.sleep(3000);
+        } catch(InterruptedException e) {
+            Log.e("EXCEPTION", "Failed to sleep");
+        }
         init();
+
+        initListeners();
+        hideLoading();
+        showControls();
+
     }
 
     /**
@@ -117,10 +146,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void init() {
-        tryBluetoothButton = (Button) findViewById(R.id.tryBluetoothButton);
+        //tryBluetoothButton = (Button) findViewById(R.id.);
+       // initListeners();
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        boolean connectedToRobot = false;
 
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -132,7 +160,6 @@ public class MainActivity extends AppCompatActivity {
             Log.i("myStuff", "Bluetooth Already Enabled");
             Toast toast = Toast.makeText(this, "Bluetooth Already Enabled", Toast.LENGTH_LONG);
             toast.show();
-
         }
 
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
@@ -141,8 +168,7 @@ public class MainActivity extends AppCompatActivity {
                 if (device.getName().equals("13516c2499dc")) {
                     mmDevice = device;
                     Log.i("myStuff", "Device equals " + device.getName());
-                    connectedToRobot = true;
-                    Toast toast = Toast.makeText(this, "Device equals " + device.getName(), Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(this, "Device equals " + device.getName(), Toast.LENGTH_SHORT);
                     toast.show();
                     break;
                 } else {
@@ -153,49 +179,161 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        if(connectedToRobot) {
+        try {
+            mmSocket = (BluetoothSocket) mmDevice.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(mmDevice, 1);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-            try {
-                mmSocket = (BluetoothSocket) mmDevice.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(mmDevice, 1);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-            Log.d("DEBUG CRASH", "mmSocket");
-            Class<?> clazz = mmSocket.getRemoteDevice().getClass();
-            Log.d("DEBUG CRASH", "clazz");
-            Class<?>[] paramTypes = new Class<?>[]{Integer.TYPE};
-            Log.d("DEBUG CRASH", "paramTypes");
-            Method m = null;
-            try {
-                m = clazz.getMethod("createRfcommSocket", paramTypes);
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-            Log.d("DEBUG CRASH", "getMethod");
-            Object[] params = new Object[]{Integer.valueOf(1)};
-            Log.d("DEBUG CRASH", "newobject[]");
+        Class<?> clazz = mmSocket.getRemoteDevice().getClass();
+        Class<?>[] paramTypes = new Class<?>[]{Integer.TYPE};
+        Method m = null;
 
-            fallbackSocket = null;
-            try {
-                fallbackSocket = (BluetoothSocket) m.invoke(mmSocket.getRemoteDevice(), params);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-            Log.d("DEBUG CRASH", "fallbackSocket");
-            try {
-                fallbackSocket.connect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            m = clazz.getMethod("createRfcommSocket", paramTypes);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        Object[] params = new Object[]{Integer.valueOf(1)};
+
+        fallbackSocket = null;
+        try {
+            fallbackSocket = (BluetoothSocket) m.invoke(mmSocket.getRemoteDevice(), params);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG CRASH", "fallbackSocket");
+        try {
+            fallbackSocket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void hideControls(){
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.controls);
+        layout.setVisibility(View.GONE);
+    }
+
+    public void showControls(){
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.controls);
+        layout.setVisibility(View.VISIBLE);
+    }
+
+    public void showLoading(){
+        ImageView ImageView = (ImageView) findViewById(R.id.forward);
+        ImageView.setVisibility(View.VISIBLE);
+    }
+
+    public void hideLoading(){
+        ImageView ImageView = (ImageView) findViewById(R.id.forward);
+        //ImageView.setVisibility(View.GONE);
+    }
+
+   /* public void loadLoadingGif(){
+        ImageView ImageView = (ImageView) findViewById(R.id.imageView3);
+        ImageView.getSettings().setJavaScriptEnabled(true);
+        ImageView.setImageViewClient(new ImageViewClient());
+        ImageView.loadUrl("http://www.lettersmarket.com/uploads/lettersmarket/blog/loaders/common_blue/ajax_loader_blue_128.gif");
+        ImageView.setVisibility(View.VISIBLE);
+    }*/
+
+    public void initListeners() {
+        forward = (ImageView) findViewById(R.id.forward);
+        backward = (ImageView) findViewById(R.id.backward);
+        turnRight = (ImageView) findViewById(R.id.turnRight);
+        turnLeft = (ImageView) findViewById(R.id.turnLeft);
+
+        forward.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action  = event.getAction();
+                switch(action) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendCommand(getResources().getString(R.string.forward));
+                        Toast.makeText(getApplicationContext(), "Forward", Toast.LENGTH_SHORT).show();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        sendCommand(getResources().getString(R.string.stopMotors));
+                        Toast.makeText(getApplicationContext(), "Stop motors", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        turnRight.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action  = event.getAction();
+                switch(action) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendCommand(getResources().getString(R.string.turnRight));
+                        Toast.makeText(getApplicationContext(), "Forward", Toast.LENGTH_SHORT).show();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        sendCommand(getResources().getString(R.string.stopMotors));
+                        Toast.makeText(getApplicationContext(), "Stop motors", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        backward.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action  = event.getAction();
+                switch(action) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendCommand(getResources().getString(R.string.backward));
+                        Toast.makeText(getApplicationContext(), "backward", Toast.LENGTH_SHORT).show();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        sendCommand(getResources().getString(R.string.stopMotors));
+                        Toast.makeText(getApplicationContext(), "Stop motors", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        turnLeft.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action  = event.getAction();
+                switch(action) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendCommand(getResources().getString(R.string.turnLeft));
+                        Toast.makeText(getApplicationContext(), "Forward", Toast.LENGTH_SHORT).show();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        sendCommand(getResources().getString(R.string.stopMotors));
+                        Toast.makeText(getApplicationContext(), "Stop motors", Toast.LENGTH_SHORT).show();
+                        sendCommand(getResources().getString(R.string.stopMotors));
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+
     }
 }
